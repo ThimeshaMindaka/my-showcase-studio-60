52a840aa3eacc0459d8abe04a5b8b7c8310447eb
Thimesha Mindaka Weliwita <87875224+ThimeshaMindaka@users.noreply.github.com>
2025-12-01 15:16:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ base: '/my-showcase-studio-60/' }) => ({
   server: {
     host: "::",
     port: 8080,
